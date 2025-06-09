d45b9152e864107ea3e99e490f31147bc2575ebe
Leandro Ucuamba <leandrodossantos.lc5@gmail.com>
2025-06-09 16:42:36 +0100

    Add font awesome in project

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,12 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { Tarefa } from '../../../tarefa';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-task-item',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css'
 })
 export class TaskItemComponent {
    @Input() tarefa!: Tarefa;
+
+   faTimes = faTimes;
 }
